133647789d2e1f151275b7287ca15f773c2c1e1a
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-06-01 17:16:40 +0600

    Added json decoding func

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -20,9 +20,20 @@ struct WeatherManager {
         
         if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url){
+                (data,  response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safetData = data {
+//                    let dataString = String(data: safetData , encoding: .utf8)
+//                    print(dataString)
+                    self.parseJSON(weatherData: safetData)
+                }
+            }
             task.resume()
-    }
+        }
     }
     
     func handle(data: Data?, response: URLResponse?, error: Error?){
@@ -31,8 +42,22 @@ struct WeatherManager {
             return
         }
         if let safetData = data {
-            let dataString = String(data: safetData , encoding: .utf8)
-            print(dataString)
+            self.parseJSON(weatherData: safetData)
+           
+        }
+    }
+    
+    func parseJSON(weatherData: Data){
+        let decoder = JSONDecoder()
+       
+        do {
+        let decodedData =  try  decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.name)
+            print(decodedData.weather[0].description)
+           
+            
+        } catch{
+            print(error)
         }
     }
 }
