b4e5b18020abedc781189071d721b3f8bbcfd1dc
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-06-01 12:40:23 +0600

    added work manager, URLSession

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=dd2cd435b3cb44a7eaeaf368e86e36e7&units=metric"
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            task.resume()
+    }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safetData = data {
+            let dataString = String(data: safetData , encoding: .utf8)
+            print(dataString)
+        }
+    }
+}
